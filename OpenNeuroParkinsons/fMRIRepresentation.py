d43b2eea1b0494ff26eaf8845ae20cde458d0acf
Alan Lee <alleetw101@gmail.com>
2020-12-28 17:54:38 -0500

    Implemented basic upscaling function in upscale_slice() for 2d images

--- OpenNeuroParkinsons/fMRIRepresentation.py
+++ OpenNeuroParkinsons/fMRIRepresentation.py
@@ -8,12 +8,14 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
+import tensorflow as tf
 
 
 path = 'ds001907/sub-RC4101/ses-1/anat/sub-RC4101_ses-1_T1w.nii.gz'
 
 img = sitk.ReadImage(path)
 data = sitk.GetArrayFromImage(img)
+data = tf.expand_dims(data, -1)
 
 
 def axial_slices3d(data_array) -> np.ndarray:
@@ -21,23 +23,44 @@ def axial_slices3d(data_array) -> np.ndarray:
 
 
 def coronal_slices3d(data_array) -> np.ndarray:
-    transposed = np.transpose(data_array, [2, 1, 0])
+    transposed = np.transpose(data_array, [2, 1, 0, 3])
     return np.flip(transposed, axis=[0, 1])
 
 
 def sagittal_slices3d(data_array) -> np.ndarray:
     return np.flip(data_array, axis=1)
 
+
+def upscale_slice(image) -> np.ndarray:
+    output_height = (len(image) * 2) - 1
+    output_width = (len(image[0]) * 2) - 1
+    output_image = np.zeros([output_height, output_width, 1])
+
+    for height_index in range(len(image)):
+        for width_index in range(len(image[0])):
+            output_image[height_index * 2][width_index * 2] = image[height_index][width_index]
+
+    for height_index in range(len(image)):
+        for width_index in range(len(image[0]) - 1):
+            output_image[height_index * 2][(width_index * 2) + 1][0] = (output_image[height_index * 2][(width_index * 2)][0] + output_image[height_index * 2][(width_index * 2 + 2)][0]) / 2.0
+
+    for height_index in range(len(image) - 1):
+        for width_index in range(output_width):
+            output_image[height_index * 2 + 1][width_index][0] = (output_image[height_index * 2][width_index][0] + output_image[(height_index * 2) + 2][width_index][0]) / 2.0
+
+    return output_image
+
+
 # Singular 2D Representation using matplotlib.pyplot
-# plt.figure(figsize=(5, 5))
-# plt.imshow(axial_slices3d(mri_image)[110], cmap='gray')
+plt.figure(figsize=(5, 5))
+plt.imshow(upscale_slice(axial_slices3d(data)[130]), cmap='gray')
 # plt.suptitle("axial, flip(axis=1) @ slice 110\nHigher index is MRI left")
 # plt.ylabel("Front")
 # plt.xlabel("Bottom")
-# plt.show()
+plt.show()
 
-# axial = sagittal_slices3d(mri_image)
-#
+
+# axial = sagittal_slices3d(data)
 # plt.figure(figsize=(10, 10))
 # for i in range(16):
 #     plt.subplot(4, 4, i + 1)

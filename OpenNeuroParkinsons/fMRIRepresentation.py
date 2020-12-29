e9448f46a7e6fb92442b4bcb801e83012a38e9b0
Alan Lee <alleetw101@gmail.com>
2020-12-28 22:27:30 -0500

    Optimized basic upscaling function in upscale_slice() for 2d images

--- OpenNeuroParkinsons/fMRIRepresentation.py
+++ OpenNeuroParkinsons/fMRIRepresentation.py
@@ -10,12 +10,14 @@ import matplotlib.pyplot as plt
 import SimpleITK as sitk
 import tensorflow as tf
 
+import time
 
 path = 'ds001907/sub-RC4101/ses-1/anat/sub-RC4101_ses-1_T1w.nii.gz'
 
 img = sitk.ReadImage(path)
 data = sitk.GetArrayFromImage(img)
 data = tf.expand_dims(data, -1)
+data /= np.amax(data)
 
 
 def axial_slices3d(data_array) -> np.ndarray:
@@ -35,14 +37,43 @@ def upscale_slice(image) -> np.ndarray:
     output_height = (len(image) * 2) - 1
     output_width = (len(image[0]) * 2) - 1
     output_image = np.zeros([output_height, output_width, 1])
+    image_height = len(image)
+    image_width = len(image[0])
 
-    for height_index in range(len(image)):
-        for width_index in range(len(image[0])):
-            output_image[height_index * 2][width_index * 2] = image[height_index][width_index]
+    # for height_index in range(len(image)):
+    #     for width_index in range(len(image[0])):
+    #         output_image[height_index * 2][width_index * 2] = image[height_index][width_index]
 
-    for height_index in range(len(image)):
-        for width_index in range(len(image[0]) - 1):
-            output_image[height_index * 2][(width_index * 2) + 1][0] = (output_image[height_index * 2][(width_index * 2)][0] + output_image[height_index * 2][(width_index * 2 + 2)][0]) / 2.0
+    row_nonzeros = []
+    height_nonzeros = list(range(image_height))
+
+    for height_index in range(image_height):
+        templist = np.nonzero(image[height_index])[0]
+        row_nonzeros.append(templist if len(templist) != 0 and templist[-1] != (image_width - 1) else templist[:-1])
+
+    for height_index in range(image_height):
+        if not row_nonzeros[height_index].any():
+            height_nonzeros.remove(height_index)
+
+    for x in np.transpose(np.nonzero(image)):
+        output_image[x[0] * 2][x[1] * 2] = image[x[0]][x[1]]
+
+    for height_index in height_nonzeros:
+        for indices in row_nonzeros[height_index]:
+            output_image[height_index * 2][(indices * 2) + 1][0] = (output_image[height_index * 2][
+                                                                        (indices * 2)][0] +
+                                                                    output_image[height_index * 2][
+                                                                        (indices * 2 + 2)][0]) / 2.0
+
+    # for height_index in height_nonzeros:
+    #     for indices in np.unique(np.concatenate((row_nonzeros[height_index] * 2, row_nonzeros[height_index + 1] * 2, (row_nonzeros[height_index] * 2) + 1, (row_nonzeros[height_index + 1] * 2) + 1, row_nonzeros[height_index][0] - 1, row_nonzeros[height_index][0] - 1))):
+    #         output_image[height_index * 2 + 1][indices][0] = (output_image[height_index * 2][indices][0] +
+    #                                                           output_image[(height_index * 2) + 2][indices][
+    #                                                               0]) / 2.0
+
+    # for height_index in range(len(image)):
+    #     for width_index in range(len(image[0]) - 1):
+    #         output_image[height_index * 2][(width_index * 2) + 1][0] = (output_image[height_index * 2][(width_index * 2)][0] + output_image[height_index * 2][(width_index * 2 + 2)][0]) / 2.0
 
     for height_index in range(len(image) - 1):
         for width_index in range(output_width):
@@ -51,14 +82,39 @@ def upscale_slice(image) -> np.ndarray:
     return output_image
 
 
+def upscale_volume(image) -> np.ndarray:
+    output_depth = (len(image) * 2) - 1
+    output_height = (len(image[0]) * 2) - 1
+    output_width = (len(image[0][0]) * 2) - 1
+    output_image = np.zeros([output_depth, output_height, output_width, 1])
+
+    for depth_index in range(len(image)):
+        start = time.time()
+        output_image[depth_index * 2] = upscale_slice(image[depth_index])
+        print(time.time() - start)
+        print(depth_index)
+
+    return output_image
+
+
 # Singular 2D Representation using matplotlib.pyplot
-plt.figure(figsize=(5, 5))
-plt.imshow(upscale_slice(axial_slices3d(data)[130]), cmap='gray')
+start = time.time()
+pltimage = upscale_slice(axial_slices3d(data)[130])
+print(time.time() - start)
+
+start = time.time()
+pltimage = upscale_slice(sagittal_slices3d(data)[130])
+print(time.time() - start)
+# pltimage = upscale_volume(sagittal_slices3d(data))[130]
+plt.figure(figsize=(10, 10))
+plt.imshow(pltimage, cmap='gray')
 # plt.suptitle("axial, flip(axis=1) @ slice 110\nHigher index is MRI left")
 # plt.ylabel("Front")
 # plt.xlabel("Bottom")
 plt.show()
-
+print(type(pltimage))
+print(pltimage.dtype)
+print(pltimage.shape)
 
 # axial = sagittal_slices3d(data)
 # plt.figure(figsize=(10, 10))

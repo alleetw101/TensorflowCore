d9a11b178a7d7edbd29a1baef56f29c049e51c8c
Alan Lee <alleetw101@gmail.com>
2020-12-30 22:40:22 -0500

    Optimizations

--- Development/OpenNeuroDS003434newbi4fmri2020/MRIImagesExportDS003434.py
+++ Development/OpenNeuroDS003434newbi4fmri2020/MRIImagesExportDS003434.py
@@ -8,7 +8,7 @@ from PIL import Image
 import MRIProcessingDS003434
 
 data = MRIProcessingDS003434.load_mri_scan('sub-01-ses-01-anat-sub-01_ses-01_T1w.nii.gz', denoise=True, expand_dims=False, denoise_upper=0.35)
-data = MRIProcessingDS003434.sagittal_slices3d(data)
+data = MRIProcessingDS003434.sagittal_slices(data)
 data *= 255
 
 for index in range(len(data)):

627cacb50eb3df3983018fe98dc713f68db69a0d
Alan Lee <alleetw101@gmail.com>
2020-12-30 22:39:39 -0500

    Optimizations and addition of loading entire dataset (ds003434)

--- Development/OpenNeuroDS003434newbi4fmri2020/MRIProcessingDS003434.py
+++ Development/OpenNeuroDS003434newbi4fmri2020/MRIProcessingDS003434.py
@@ -2,16 +2,21 @@
 # https://github.com/alleetw101/TensorflowCore <2020>
 #
 # fMRI/MRI representations using dataset from https://openneuro.org/datasets/ds003434/versions/1.0.1
-#
+# Numpy array of fmri dataset (5 runs per subject) contains 9e9 elements
+# Numpy array of mri dataset contains 2e8 elements
 
 import numpy as np
 import matplotlib.pyplot as plt
 import SimpleITK as sitk
+import os
+import time
+
+from PIL import Image
 
 
-def load_mri_scan(filepath: str, pad: bool = True, pad_shape=None, normalize: bool = True,
-                  normalize_range=(0, 1), expand_dims: bool = True, fmri_denoise: bool = False,
-                  fmri_denoise_level: int = 0.01) -> np.ndarray:
+def load_mri_scan(filepath: str, pad: bool = True, pad_shape=None, normalize: bool = True, normalize_range=(0, 1),
+                  expand_dims: bool = True, denoise: bool = False, denoise_lower: float = 0.05,
+                  denoise_upper: float = 0.45) -> np.ndarray:
     output = sitk.GetArrayFromImage(sitk.ReadImage(filepath))
 
     if not (output.ndim == 3 or output.ndim == 4):
@@ -31,15 +36,23 @@ def load_mri_scan(filepath: str, pad: bool = True, pad_shape=None, normalize: bo
             if pad_shape[index] - output.shape[index] >= 0:
                 pad_array.append((0, pad_shape[index] - output.shape[index]))
             else:
-                raise ValueError(f'Padding (pad_shape) must be greater than the original image in dimension {index}')
+                shape = list(output.shape)
+                shape[index] = pad_shape[index]
+                if output.ndim == 3:
+                    output = output[:shape[0], :shape[1], :shape[2]]
+                else:
+                    output = output[:shape[0], :shape[1], :shape[2], :shape[3]]
+                pad_array.append((0, pad_shape[index] - output.shape[index]))
 
         output = np.pad(output, pad_array, mode='constant', constant_values=0.0)
 
     if normalize:
         output = output / np.amax(output)
 
-        if output.ndim == 4 and fmri_denoise:
-            output[output < fmri_denoise_level] = 0.0
+        if denoise:
+            output[output < denoise_lower] = 0.0
+            output[output > denoise_upper] = 0.0
+            output = output / np.amax(output)
 
         if normalize_range != (0, 1):
             output *= (normalize_range[1] - normalize_range[0])
@@ -51,7 +64,7 @@ def load_mri_scan(filepath: str, pad: bool = True, pad_shape=None, normalize: bo
     return output
 
 
-def plot_slice(data_array: np.ndarray, slice_index: int, time_index: int = 0, step_size: int = 10, figsize=(10, 10),
+def plot_slice(data_array: np.ndarray, slice_index: int, time_index: int = 0, step_size: int = 10, figsize=(10, 9),
                vplots: int = 1, hplots: int = 1, cmap: str = 'gray'):
     plt.figure(figsize=figsize)
 
@@ -75,7 +88,7 @@ def plot_slice(data_array: np.ndarray, slice_index: int, time_index: int = 0, st
     plt.show()
 
 
-def axial_slices3d(data_array) -> np.ndarray:  # Hiegh [-3] index is superior
+def axial_slices(data_array) -> np.ndarray:  # Hiegh [-3] index is superior
     if data_array.ndim == 3:
         output = np.transpose(data_array, [1, 2, 0])
         output = np.flip(output, axis=0)
@@ -90,7 +103,7 @@ def axial_slices3d(data_array) -> np.ndarray:  # Hiegh [-3] index is superior
     return output
 
 
-def coronal_slices3d(data_array) -> np.ndarray:  # Higher [-3] index is anterior
+def coronal_slices(data_array) -> np.ndarray:  # Higher [-3] index is anterior
     if data_array.ndim == 3:
         output = np.transpose(data_array, [2, 1, 0])
         output = np.flip(output, axis=[0])
@@ -110,7 +123,7 @@ def coronal_slices3d(data_array) -> np.ndarray:  # Higher [-3] index is anterior
     return output
 
 
-def sagittal_slices3d(data_array) -> np.ndarray:  # Higher index is MRI left (POV right)
+def sagittal_slices(data_array) -> np.ndarray:  # Higher index is MRI left (POV right)
     if data_array.ndim == 3 or (data_array.ndim == 4 and data_array.shape[-1] <= 3):
         output = data_array
     elif data_array.ndim == 4:
@@ -126,13 +139,111 @@ def sagittal_slices3d(data_array) -> np.ndarray:  # Higher index is MRI left (PO
     return output
 
 
-path = 'sub-01-ses-01-anat-sub-01_ses-01_T1w.nii.gz'
-fmripath = 'sub-01-ses-01-func-sub-01_ses-01_task-MainExp_run-01_bold.nii.gz'
+def process_png_overlay(filepath: str) -> np.ndarray:
+    overlay = np.array(Image.open(filepath))
 
-# data = load_mri_scan(path)
-# data = sagittal_slices3d(data)
-# plot_slice(data, slice_index=110)
+    overlay = overlay[:, :, :1]
+    overlay[overlay < 3] = 0
+    overlay[overlay > 239] = 0
 
-data = load_mri_scan(fmripath)
-data = sagittal_slices3d(data)
-plot_slice(data, slice_index=50)
+    return overlay
+
+
+def load_dataset(filepath: str, fmri: bool = False, loading_status: bool = True, pad: bool = True, pad_shape=None,
+                 normalize: bool = True, normalize_range=(0, 1), expand_dims: bool = True, denoise: bool = False,
+                 denoise_lower: float = 0.05, denoise_upper: float = 0.45) -> np.ndarray:
+    scan_paths = list(map(lambda x: os.path.join(filepath, x, 'ses-01'),
+                          [f for f in os.listdir(filepath) if 'sub' in f]))
+    output = []
+    fmri_list = []
+
+    if fmri:
+        scan_paths = (list(map(lambda x: os.path.join(x, 'func'), scan_paths)))
+        scan_paths.sort()
+
+        for path in scan_paths:
+            temp_list = [os.path.join(path, f) for f in os.listdir(path) if '.nii' in f]
+            temp_list.sort()
+            fmri_list.append(temp_list[:5])
+
+        for path_lists in fmri_list:
+            temp_list = []
+            for run_paths in path_lists:
+                starttime = time.time()
+                temp_list.append(load_mri_scan(run_paths, pad=pad, pad_shape=pad_shape, normalize=normalize,
+                                               normalize_range=normalize_range, expand_dims=expand_dims,
+                                               denoise=denoise, denoise_lower=denoise_lower,
+                                               denoise_upper=denoise_upper))
+                if loading_status:
+                    print(f'Loaded ({time.time() - starttime:0.3}s): ' + run_paths)
+            output.append(temp_list)
+    else:
+        scan_paths = list(map(lambda x: os.path.join(x, 'anat'), scan_paths))
+        scan_paths = list(map(lambda x: os.path.join(x, [f for f in os.listdir(x) if '.nii' in f][0]), scan_paths))
+        scan_paths.sort()
+        for scanpath in scan_paths:
+            starttime = time.time()
+            output.append(load_mri_scan(scanpath, pad=pad, pad_shape=pad_shape, normalize=normalize,
+                                        normalize_range=normalize_range, expand_dims=expand_dims, denoise=denoise,
+                                        denoise_lower=denoise_lower, denoise_upper=denoise_upper))
+            if loading_status:
+                print(f'Loaded ({time.time() - starttime:0.3}s): ' + scanpath)
+
+    return np.array(output)
+
+
+def testingoverlay():
+    data = load_mri_scan('sub-01-ses-01-anat-sub-01_ses-01_T1w.nii.gz', denoise=True)
+    data = sagittal_slices(data)
+
+    overlay = process_png_overlay('11080.png')
+
+    print(overlay.shape)
+
+    plt.figure(figsize=(10, 9))
+    plt.imshow(data[80], cmap='gray')
+    plt.imshow(overlay, alpha=0.8)
+    plt.show()
+
+
+def testingloaddataset():
+    path = 'ds003434'
+    filepath = 'ds003434/sub-14/ses-01/anat/sub-14_ses-01_T1w.nii.gz'
+
+    # data = load_mri_scan(filepath, denoise=True)
+    # print(data.shape)
+    # plot_slice(data, slice_index=110)
+
+    dataset = load_dataset(path, fmri=True)
+    print(dataset.shape)
+    # plot_slice(dataset[10], slice_index=110)
+    # plt.figure(figsize=(10, 9))
+    # for num in range(16):
+    #     plt.subplot(4, 4, num + 1)
+    #     plt.imshow(dataset[num + 2][83], cmap='gray')
+    #     plt.title(num + 2)
+    #
+    # plt.show()
+
+
+testingloaddataset()
+
+
+def generaltesting():
+    path = 'sub-01-ses-01-anat-sub-01_ses-01_T1w.nii.gz'
+    fmripath = 'sub-01-ses-01-func-sub-01_ses-01_task-MainExp_run-01_bold.nii.gz'
+
+    data = load_mri_scan(path, denoise=True, denoise_lower=0.07, denoise_upper=0.35, normalize=True)
+    data = sagittal_slices(data)
+    plot_slice(data, slice_index=10, vplots=4, hplots=4, step_size=10)
+
+    plt.figure(figsize=(10, 9))
+    plt.hist(data.flatten())
+    plt.show()
+    # data = load_mri_scan(fmripath)
+    # data = sagittal_slices(data)
+    # plot_slice(data, slice_index=50)
+
+    print(np.std(data[data != 0.0]))
+    print(np.mean(data))
+    print(data[100][100])

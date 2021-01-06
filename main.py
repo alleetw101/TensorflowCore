78800a1251746e0b142723c2a383dbfec51bbe02
Alan Lee <alleetw101@gmail.com>
2021-01-05 20:59:58 -0500

    updated

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
+from euroSAT import euroSATModel
+import tensorflow
 
 def print_hi(name):
     print(f'Hi, {name}')
 
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    sample = tensorflow.keras.models.load_model('euroSAT/euroSAT_SavedModel20201223')
+    sample.summary()
 

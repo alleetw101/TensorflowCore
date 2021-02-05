58af5524eb16ccb2bd1c967290e702c199e2c796
Alan Lee <alleetw101@gmail.com>
2021-02-04 20:33:25 -0500

    Reorganization

--- OpenNeuro/MRIImageSegmentationExport.py
+++ OpenNeuro/MRIImageSegmentationExport.py
@@ -11,27 +11,27 @@ model = tf.keras.models.load_model('/Users/alan/Documents/Programming/Python/Ten
 
 sample_path = '/Users/alan/Documents/Programming/Python/TensorflowCore/OpenNeuro/OpenNeuroDS003434newbi4fmri2020/ds003434/sub-02/ses-01/anat/sub-02_ses-01_T1w.nii.gz'
 sample = niftiProcessing.load_mri_scan(sample_path, denoise=True)
-sample = np.stack((sample,)*3, axis=-2)
-sample = np.squeeze(sample, axis=-1)
-sample_image = sample[20]
-print(sample_image.shape)
+sample = np.concatenate((sample,)*3, axis=-1)
+sample_image = sample[86]
 prediction = model.predict(sample_image[tf.newaxis, ...])
-print(prediction[0][124])
-# prediction += abs(np.amin(prediction))
-# prediction /= np.amax(prediction)
 
 prediction[prediction < 0] = 0
-prediction[prediction > 0] = 1
+prediction[prediction > 0] = 255
+prediction = prediction[0][..., :1]
 
-# prediction = np.ma.masked_where(prediction == 0, prediction)
+prediction = np.ma.masked_where(prediction == 0, prediction)
 
-plt.figure(figsize=(9,9))
+plt.figure(figsize=(9, 9))
 plt.xticks([])
 plt.yticks([])
-plt.imshow(prediction[0])
 plt.imshow(sample_image, cmap='gray')
-plt.imshow(prediction[0], alpha=0.5)
+plt.imshow(prediction, alpha=0.5)
 plt.show()
 
-print(prediction[0][124])
-print(sample_image[124])
+
+sample_image = sample_image[..., :1]
+png = np.concatenate((sample_image, prediction), axis=-1)
+print(prediction.shape)
+print(sample_image.shape)
+print(png.shape)
+print(png[80])
